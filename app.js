63c68834ab5d149f832432e076eea1b752bf8cda
dbhatkar009 <dhananjay.bhatkar999@outlook.com>
2025-11-06 17:22:03 +0530

    Parsing Request Body

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 //Working on Node.js Server Creating New Node Server
 const http = require('http');
+const fs = require('fs');
+const { buffer } = require('stream/consumers');
 
 const server = http.createServer((req, res)=>{
- console.log(req.url, req.method, req.headers);
+ const url = req.url;
+ const method = req.method;
+ if(url === '/'){
+ res.write('<html>');
+ res.write('<head><title> Enter A Message </title></head>');
+ res.write('<body> <form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form> </body>');
+ res.write('</html>');
+ return res.end();    
+ }
+ if(url === "/message" && method === 'POST'){
+     const body = [];
+    req.on('data', (chunk)=>{
+       console.log(chunk); 
+       body.push(chunk);
+    });
+    req.on('end', ()=>{
+        const parseBody = Buffer.concat(body).toString();
+        const message = parseBody.split('=')[1];
+        fs.writeFileSync('message.txt',message);
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    });
+ }
  res.setHeader('Content-type', 'text/html');
  res.write('<html>');
  res.write('<head><title> this is Node Js server </title></head>');
