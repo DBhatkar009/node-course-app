6000d5959d4849dcd379b5292b6aa74c2480553a
dbhatkar009 <dhananjay.bhatkar999@outlook.com>
2025-11-07 16:56:01 +0530

    Created A Routes And installing Npm packages

--- app.js
+++ app.js
@@ -1,40 +1,11 @@
 //Working on Node.js Server Creating New Node Server
 const http = require('http');
-const fs = require('fs');
-const { buffer } = require('stream/consumers');
 
-const server = http.createServer((req, res)=>{
- const url = req.url;
- const method = req.method;
- if(url === '/'){
- res.write('<html>');
- res.write('<head><title> Enter A Message </title></head>');
- res.write('<body> <form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form> </body>');
- res.write('</html>');
- return res.end();    
- }
- if(url === "/message" && method === 'POST'){
-     const body = [];
-    req.on('data', (chunk)=>{
-       console.log(chunk); 
-       body.push(chunk);
-    });
-    req.on('end', ()=>{
-        const parseBody = Buffer.concat(body).toString();
-        const message = parseBody.split('=')[1];
-        fs.writeFileSync('message.txt',message);
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    });
- }
- res.setHeader('Content-type', 'text/html');
- res.write('<html>');
- res.write('<head><title> this is Node Js server </title></head>');
- res.write('<body> Hello there Welcome to the Node App! </body>');
- res.write('</html>');
- res.end();
-//  process.exit();
-});
+const routes = require('./routes');
+// const { buffer } = require('stream/consumers');
+
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
 
 server.listen(3000); 
