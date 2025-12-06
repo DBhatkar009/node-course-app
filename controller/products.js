4b5ba346b3f9c21475bcf4fd6d8efa42b4b2b33e
dbhatkar009 <dhananjay.bhatkar999@outlook.com>
2025-12-06 18:31:15 +0530

    storing data on json format on data folder

--- controller/products.js
+++ controller/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require("../models/product");
 
 exports.getAddProducts = (req, res, next) => {
   res.render("add-products", {
@@ -10,17 +10,20 @@ exports.getAddProducts = (req, res, next) => {
 };
 
 exports.postNewProducts = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render("shop", {
-    data: products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    productCss: true,
-    activeShop: true,
+  Product.fetchAll((products) => {
+    res.render("shop", {
+      data: products,
+      pageTitle: "Shop",
+      path: "/",
+      hasProducts: products.length > 0,
+      productCss: true,
+      activeShop: true,
+    });
   });
 };
