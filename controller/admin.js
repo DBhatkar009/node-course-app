d8324706abc5bf113eabf698b21fd6e13f99b6bf
dbhatkar009 <dhananjay.bhatkar999@outlook.com>
2026-01-08 18:44:44 +0530

    implimented edit method with save updated data

--- controller/admin.js
+++ controller/admin.js
@@ -25,9 +25,9 @@ exports.getAddProducts = (req, res, next) => {
 exports.postNewProducts = (req, res, next) => {
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
-  const description = req.body.description;
   const price = req.body.price;
-  const product = new Product(title, imageUrl, description, price);
+  const description = req.body.description;
+  const product = new Product(null, title, imageUrl, price, description);
   product.save();
   res.redirect("/");
 };
@@ -59,6 +59,20 @@ exports.getEditProducts = (req, res, next) => {
 
 exports.postEditProducts = (req, res, next) => {
   // to be implemented
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedPrice = req.body.price;
+  const updatedDescription = req.body.description;
+  const updatedData = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedPrice,
+    updatedDescription
+  );
+  updatedData.save();
+  res.redirect("/add-product-list");
 };
 
 /*
